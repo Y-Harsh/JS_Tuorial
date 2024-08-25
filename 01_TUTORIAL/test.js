1c1b66dc53693f6918a544cebd9ccb364cf436d6
killer-knight <167885456+killer-knight@users.noreply.github.com>
2024-08-25 09:12:32 +0000

    data_types

--- 01_TUTORIAL/test.js
+++ 01_TUTORIAL/test.js
@@ -1,1 +1,30 @@
-console.log("Hare Krishna")
+// console.log("Hare Krishna")
+
+// const accountId=144553
+// let accountEmail="hitesh@google.com"
+// var accountPassword="12345"
+// accountCity="jaipur"
+// let accountState
+
+// accountID=2
+// console.log(accountId); 
+// accountEmail="jwjbefkwqldsqf"
+// accountPassword="59804"
+// accountCity="jcoaver"
+
+// console.log(accountId);
+// console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+
+"use strict"; 
+// treat all JS code as newer version
+// alert(3+3) we are using node js not browser so stop   
+
+let name="krishna"
+let age=1e9
+let isAlive=true
+
+console.log(typeof null);
+console.log(typeof name);
+console.log(typeof age);
